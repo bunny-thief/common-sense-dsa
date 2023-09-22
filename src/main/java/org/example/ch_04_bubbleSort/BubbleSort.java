ac7d239faa66d5af0ed2ba8bbac17f75555c3026
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-09-22 23:28:51 +0200

    Decrement index
    Moves index after for loop. This stops the swaps
    from going all the way to the last number since it's
    in the right place.

--- src/main/java/org/example/ch_04_bubbleSort/BubbleSort.java
+++ src/main/java/org/example/ch_04_bubbleSort/BubbleSort.java
@@ -7,16 +7,20 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] nums = {10, 1, 9, 2, 8, 3, 7, 4, 6, 5};
         bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
     public static void bubbleSort(int[] array) {
         boolean isSorted = true;
+        int lastIndex = array.length - 1;
+
         while (isSorted) {
             isSorted = false;
-            for (int i = 0; i < array.length - 1; i++) {
+
+            for (int i = 0; i < lastIndex; i++) {
                 isSorted = swapNumbers(array, isSorted, i);
             }
-            System.out.println(Arrays.toString(array));
+            lastIndex--;
         }
     }
 
