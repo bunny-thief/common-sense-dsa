4e338c02b2d54c8a8de7985fb7f4222eb1b7f865
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-10-01 22:13:08 +0200

    Add Queue class
    (ch09)

---
+++ src/main/java/org/example/ch_09_stacksQueues/Queue.java
@@ -0,0 +1,40 @@
+package org.example.ch_09_stacksQueues;
+
+import org.example.ch_14_nodeBasedDataStructures.LinkedList;
+import org.example.ch_14_nodeBasedDataStructures.Node;
+
+public class Queue {
+
+    private LinkedList queue;
+    private Node head;
+    private Node tail;
+
+    public Queue() {
+        queue = new LinkedList();
+        head = queue.getFirst();
+        tail = queue.getLast();
+    }
+
+    public Queue(LinkedList queue) {
+        this.queue = queue;
+        head = queue.getFirst();
+        tail = queue.getLast();
+    }
+
+    private Node getHead() {
+        return head;
+    }
+
+    private void setHead(Node head) {
+        this.head = head;
+    }
+
+    private Node getTail() {
+        return tail;
+    }
+
+    private void setTail(Node tail) {
+        this.tail = tail;
+    }
+
+}
