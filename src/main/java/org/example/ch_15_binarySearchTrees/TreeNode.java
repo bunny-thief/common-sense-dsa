09d85741ead6e152b315eb021b29609d0fb6810b
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-10-01 20:40:13 +0200

    Add TreeNode class
    (ch15)

---
+++ src/main/java/org/example/ch_15_binarySearchTrees/TreeNode.java
@@ -0,0 +1,42 @@
+package org.example.ch_15_binarySearchTrees;
+
+public class TreeNode {
+
+    private int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d)", getData());
+    }
+
+}
